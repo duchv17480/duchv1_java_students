d45105e5d17fbc4dc82d2ee30e07a1cc9026a270
Hoang Viet Duc <duchv1@hybrid-technologies.vn>
2022-09-06 16:32:59 +0700

    update interceptor

--- src/main/java/com/hybird/admin/controller/MarkController.java
+++ src/main/java/com/hybird/admin/controller/MarkController.java
@@ -3,6 +3,7 @@ package com.hybird.admin.controller;
 import com.hybird.entities.Marks;
 import com.hybird.entities.Students;
 import com.hybird.entities.Users;
+import com.hybird.repository.MarksRepository;
 import com.hybird.repository.StudentRepository;
 import com.hybird.service.MarksService;
 import com.hybird.service.StudentService;
@@ -11,13 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("admin/marks")
@@ -30,11 +29,18 @@ public class MarkController {
     private StudentService studentService;
 
     @Autowired
+    private MarksRepository marksRepository;
+
+    @Autowired
     private SubjectService subjectService;
 
     @GetMapping
-    public String list(Model model){
-        model.addAttribute("list",marksService.findAll());
+    public String list(Model model, @RequestParam("sid") Optional<Integer> sid){
+        if (sid.isPresent()) {
+            model.addAttribute("list",marksRepository.findMarks(sid.get()));
+        }else {
+            model.addAttribute("list",marksService.findAll());
+        }
         return "/views/marks/list";
     }
     @GetMapping("create")
--- src/main/java/com/hybird/repository/MarksRepository.java
+++ src/main/java/com/hybird/repository/MarksRepository.java
@@ -2,8 +2,13 @@ package com.hybird.repository;
 
 import com.hybird.entities.Marks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MarksRepository extends JpaRepository<Marks,Integer> {
+    @Query("SELECT m from Marks m where m.subjectsId.id=?1")
+    List<Marks> findMarks(int sid);
 }
